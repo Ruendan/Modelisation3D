bb079b8c0a5b3835f003d01595bb68199ab38b3c
duboisq <duboisq@bouleau21.iut-infobio.priv.univ-lille1.fr>
2020-09-15 11:04:04 +0200

    sqrgefd

--- sources-du-projet/src/test/Test.java
+++ sources-du-projet/src/test/Test.java
@@ -1,9 +1,13 @@
 package test;
 
 public class Test {
+
 	private boolean joke;
 	public void testmethode(){
 		joke = true;
 		joke = false;
+		int a = 1;
+		String aligator = "";
+		System.out.println(aligator);
 	}
 }
