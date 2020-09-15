086331e0148e0f694941dbda1f204a922dd4c0a7
Marmotte <Marmotte@192.168.0.42>
2020-09-15 18:08:54 +0200

    Quentin : First Test. Pensez a import JUNIT4,

---
+++ sources-du-projet/src/modele/Tested.java
@@ -0,0 +1,30 @@
+package modele;
+
+public class Tested {
+	
+	private boolean enCours;
+	
+	public Tested() {
+		enCours = false;
+		System.out.println("The project is ready to begin !");
+	}
+	
+	public void start() {
+		enCours = true;
+		display();
+	}
+	
+	public void stop() {
+		enCours = false;
+		display();
+	}
+	
+	public boolean isRunning(){
+		display();
+		return enCours;
+	}
+	
+	public void display() {
+		System.out.println("The project is " + (enCours?"Running !":"Waiting..."));
+	}
+}
---
+++ sources-du-projet/test/modele/TestTested.java
@@ -0,0 +1,45 @@
+package modele;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestTested {
+	
+	Tested t;
+	
+	@Before
+	public void initialize() {
+		t = new Tested();
+	}
+
+	@Test
+	public void testTest() {
+		assertFalse(t==null);
+	}
+
+	@Test
+	public void testStart() {
+		assertFalse(t.isRunning());
+		t.start();
+		assertTrue(t.isRunning());
+	}
+
+	@Test
+	public void testStop() {
+		t.start();
+		assertTrue(t.isRunning());
+		t.stop();
+		assertFalse(t.isRunning());
+	}
+
+	@Test
+	public void testIsRunning() {
+		t.start();
+		assertTrue(t.isRunning());
+		t.stop();
+		assertFalse(t.isRunning());
+	}
+
+}
