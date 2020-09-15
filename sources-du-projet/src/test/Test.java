fa0104c1e6e9a3f30af308aa0fa987fa0a832364
duboisq <duboisq@bouleau21.iut-infobio.priv.univ-lille1.fr>
2020-09-15 11:44:05 +0200

    Commit sur test. Questions sur i++ ou ++i

--- sources-du-projet/src/test/Test.java
+++ sources-du-projet/src/test/Test.java
@@ -12,4 +12,23 @@ public class Test {
 		
 		System.out.println("Fuck Gitlab");
 	}
+	
+	public static void main(String[] args) {
+		System.out.println("Questions sur la diff entre i++ et ++i");
+		int i;
+		int j;
+		 
+		i=1;
+		j=i++;
+		System.out.println("i++: "+j);
+		j=i++;
+		System.out.println("i++: "+j);
+		 
+		i=1;
+		j=++i;
+		System.out.println("++i: "+j);
+		j=++i;
+		System.out.println("++i: "+j);
+		
+	}
 }
