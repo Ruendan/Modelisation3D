b02684ce550e4a205a449552e30f7972c2ca6633
Dubois Quentin <quentin.dubois3.etu@univ-lile.fr>
2020-09-30 21:21:38 +0200

    Update Ply Setters

--- sources-du-projet/src/modele/Ply.java
+++ sources-du-projet/src/modele/Ply.java
@@ -127,6 +127,82 @@ public class Ply {
 		return null;
 	}
 	
+	public static void setUrl(String url) {
+		Ply.url = url;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public void setFormat(String format) {
+		this.format = format;
+	}
+
+	public void setVertex(int vertex) {
+		this.vertex = vertex;
+	}
+
+	public void setFace(int face) {
+		this.face = face;
+	}
+
+	public void setX(double x) {
+		this.x = x;
+	}
+
+	public void setY(double y) {
+		this.y = y;
+	}
+
+	public void setZ(double z) {
+		this.z = z;
+	}
+
+	public void setNx(double nx) {
+		this.nx = nx;
+	}
+
+	public void setNy(double ny) {
+		this.ny = ny;
+	}
+
+	public void setNz(double nz) {
+		this.nz = nz;
+	}
+
+	public void setRed(String red) {
+		this.red = red;
+	}
+
+	public void setGreen(String green) {
+		this.green = green;
+	}
+
+	public void setBlue(String blue) {
+		this.blue = blue;
+	}
+
+	public void setAlpha(String alpha) {
+		this.alpha = alpha;
+	}
+
+	public void setVertex_indice_elem(int vertex_indice_elem) {
+		this.vertex_indice_elem = vertex_indice_elem;
+	}
+
+	public void setVertex_indices(List<Integer> vertex_indices) {
+		this.vertex_indices = vertex_indices;
+	}
+
+	public void setPoints(List<Point> points) {
+		this.points = points;
+	}
+
+	public void setFaces(List<Face> faces) {
+		this.faces = faces;
+	}
+
 	public static void main(String[] args) {
 		loadPly("cube");
 	}
