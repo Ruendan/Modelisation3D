aba4c8190259b17c8ebb139ac58401c379b94084
Marmotte <Marmotte@192.168.0.42>
2020-10-02 22:58:03 +0200

    Correctif movement

--- sources-du-projet/src/modele/Mouvement.java
+++ sources-du-projet/src/modele/Mouvement.java
@@ -1,41 +1,25 @@
 package modele;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Mouvement {
 	
-	private List<Point> alreadyMoved;
-	private List<Point> alreadyRotated;
-	
 	public Mouvement() {
-		alreadyMoved= new ArrayList<Point>();
-		alreadyRotated = new ArrayList<Point>();
 	}
 	
 	public void rotate(Figure f, double thetaX, double thetaY, double thetaZ) {
 		Point temp = new Point(f.getCenter().getX(), f.getCenter().getY(), f.getCenter().getZ());
 		this.deplacer(f,-f.getCenter().getX(), -f.getCenter().getY(), -f.getCenter().getZ());
 		
-		for(Face face : f.getFaces()) {
-			for(Point p : face.getPoints()) {
-				if(!alreadyRotated.contains(p)) {
-					p.rotate(thetaX, thetaY, thetaZ);
-					alreadyRotated.add(p);
-				}
-			}
+		for(Point p : f.getPoints()) {
+			p.rotate(thetaX, thetaY, thetaZ);
 		}
 		this.deplacer(f,temp.getX(), temp.getY(), temp.getZ());
 	}
 	
 	public void deplacer(Figure f, double x, double y, double z) {
-		for(Face face : f.getFaces()){
-			for(Point p : face.getPoints()) {
-				if(!alreadyMoved.contains(p)) {
-					p.deplacer(x, y, z);
-				}
-			}
+		for(Point p : f.getPoints()) {
+			p.deplacer(x, y, z);
 		}
 		f.getCenter().deplacer(x, y, z);
 	}
 }
+
