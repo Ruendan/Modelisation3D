30bdc765698066eb041bb25ae0666051513b2050
Marmotte <Marmotte@192.168.0.42>
2020-10-03 22:59:00 +0200

    Point Moyen et MoyenneX/Y/Z Tests

--- sources-du-projet/test/modele/FaceTest.java
+++ sources-du-projet/test/modele/FaceTest.java
@@ -31,15 +31,64 @@ class FaceTest {
 	}
 	
 	@Test
-	void testGetters() {
+	public void testGetters() {
 		assertEquals(4, classique.getNbPoints());
 		assertEquals(classiqueList, classique.getPoints());
 		
 		assertEquals(0, vide.getNbPoints());
 		assertEquals(videList, vide.getPoints());
 		
+		
+		//Il faut que tu gère le Null Exception dans le constructeur de Face
 		assertEquals(nulle.getNbPoints(), 0);
 		assertEquals(new ArrayList<Integer>(), nulle.getPoints());
+	}
+	
+	@Test
+	public void test_point_moyen_classique() {
+		//Indice, il faut que tu ecrive le public equals(Object o) de Face :D )
+		assertEquals(new Point(0.5, 0.5, 0), classique.pointMoyen());
+	}
+	
+	@Test
+	public void test_point_moyen_vide() {
+		assertEquals(new Point(Double.NaN, Double.NaN, Double.NaN), vide.pointMoyen());
+	}
+	
+	@Test
+	public void test_point_moyen_nulle() {
+		//Normalement, si le test getter fonctionne, alors lui aussi
+		assertEquals(new Point(Double.NaN, Double.NaN, Double.NaN), nulle.pointMoyen());
+	}
+	
+	@Test
+	public void test_moyenneX_classique() {
+		assertEquals(0.5, classique.moyenneX());
+	}
+	
+	@Test
+	public void test_moyenneY_classique() {
+		assertEquals(0.5, classique.moyenneY());
+	}
+	
+	@Test
+	public void test_moyenneZ_classique() {
+		assertEquals(0, classique.moyenneZ());
+	}
+	
+	@Test
+	public void test_moyenneX_vide() {
+		assertEquals(0, vide.moyenneX());
+	}
+	
+	@Test
+	public void test_moyenneY_vide() {
+		assertEquals(0, vide.moyenneY());
+	}
+	
+	@Test
+	public void test_moyenneZ_vide() {
+		assertEquals(0, vide.moyenneZ());
 	}
 
 }
