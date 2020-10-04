60af9b305cfd7fc6f148ae737a3a8e324bd80058
Orianna <Orianna@192.168.1.25>
2020-10-04 17:36:04 +0200

    1er jet du calcul de norm

--- sources-du-projet/src/modele/Vecteur.java
+++ sources-du-projet/src/modele/Vecteur.java
@@ -22,6 +22,32 @@ public class Vecteur {
 		this(b.getX()-a.getX(),b.getY()-a.getY(),b.getZ()-a.getZ());
 	}
 	
+	public static Vecteur getNormal(Point a,Point b, Point c) {
+		double x,y,z;
+		Vecteur ab = new Vecteur(a,b);
+		Vecteur ac = new Vecteur(a,c);
+		removeZ(ab,ac);
+		simplAC(ab,ac);
+		return null;
+	}
+	
+	private static Vecteur simplAC(Vecteur ab, Vecteur ac) {
+		double y = ac.dirX;
+		double x = -(ac.dirY);
+		double z = (ab.dirX*x+ab.dirY*y)/ab.dirZ;//TODO
+		return new Vecteur(ab.dirX,ab.dirY,ab.dirZ);
+	}
+
+	private static void removeZ(Vecteur ab, Vecteur ac) {
+		double facteur = Math.abs(ac.dirZ)/Math.abs(ab.dirZ);
+		ab.dirX*=facteur;
+		ab.dirY*=facteur;
+		ab.dirZ*=facteur;
+		ac.dirX+=ab.dirX;
+		ac.dirY+=ab.dirY;
+		ac.dirZ+=ab.dirZ;
+	}
+
 	public String toString() {
 		return ""+dirX+","+dirY+","+dirZ;
 	}
