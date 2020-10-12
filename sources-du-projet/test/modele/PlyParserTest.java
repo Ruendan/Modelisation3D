33ce41856474a5964fa23a6ef8d4794a81fa0a94
Marmotte <quentin.dubois3.etu>
2020-10-12 13:34:05 +0200

    First Commit, Et Surement le dernier. Passage de Listes à Set effectué.

--- sources-du-projet/test/modele/PlyParserTest.java
+++ sources-du-projet/test/modele/PlyParserTest.java
@@ -4,7 +4,9 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -18,6 +20,8 @@ class PlyParserTest {
 	private Ply tested;
 	private Ply expected;
 	private List<Point> expectedListPoints;
+	private Set<Point> expectedSetPoints;
+	
 	private List<Face> expectedListFaces;
 	
 	@BeforeEach
@@ -34,6 +38,10 @@ class PlyParserTest {
 			new Point( 1.0, 1.0, 1.0),
 			new Point(-1.0, 1.0, 1.0),
 		});
+		
+		expectedSetPoints = new HashSet<Point>();
+		expectedSetPoints.addAll(expectedListPoints);
+		
 		expectedListFaces = new ArrayList<Face>(Arrays.asList(new Face[] {
 				new Face(4, Arrays.asList(new Point[] {expectedListPoints.get(0), expectedListPoints.get(1), expectedListPoints.get(2), expectedListPoints.get(3)})),
 				new Face(4, Arrays.asList(new Point[] {expectedListPoints.get(5), expectedListPoints.get(4), expectedListPoints.get(7), expectedListPoints.get(6)})),
@@ -42,7 +50,7 @@ class PlyParserTest {
 				new Face(4, Arrays.asList(new Point[] {expectedListPoints.get(7), expectedListPoints.get(3), expectedListPoints.get(2), expectedListPoints.get(6)})),
 				new Face(4, Arrays.asList(new Point[] {expectedListPoints.get(5), expectedListPoints.get(1), expectedListPoints.get(0), expectedListPoints.get(4)}))
 		}));
-		expected = new Ply(8, 6, expectedListPoints, expectedListFaces);
+		expected = new Ply(8, 6, expectedSetPoints, expectedListFaces);
 		
 	}
 	
@@ -50,18 +58,24 @@ class PlyParserTest {
 	public void test_Initialisation() {
 		assertEquals(8, tested.getVertex());
 		assertEquals(6, tested.getNbFace());
-		assertEquals(expectedListPoints, tested.getPoints());
+		assertEquals(expectedSetPoints, tested.getPoints());
 		assertEquals(expectedListFaces, tested.getFaces());
 	}
 	
 	@Test
 	public void test_Same_Points_on_Face_and_Figure() {
-		assertSame(tested.getFaces().get(0).getPoints().get(0), tested.getPoints().get(0));
-		assertSame(tested.getFaces().get(1).getPoints().get(0), tested.getPoints().get(5));
-		assertSame(tested.getFaces().get(2).getPoints().get(1), tested.getPoints().get(2));
-		assertSame(tested.getFaces().get(3).getPoints().get(2), tested.getPoints().get(4));
-		assertSame(tested.getFaces().get(4).getPoints().get(3), tested.getPoints().get(6));
-		assertSame(tested.getFaces().get(5).getPoints().get(3), tested.getPoints().get(4));
+		
+		assertTrue(tested.getPoints().contains(tested.getFaces().get(0).getPoints().get(0)));
+		
+		assertTrue(tested.getPoints().contains(tested.getFaces().get(1).getPoints().get(0)));
+		
+		assertTrue(tested.getPoints().contains(tested.getFaces().get(2).getPoints().get(1)));
+		
+		assertTrue(tested.getPoints().contains(tested.getFaces().get(3).getPoints().get(2)));
+		
+		assertTrue(tested.getPoints().contains(tested.getFaces().get(4).getPoints().get(3)));
+		
+		assertTrue(tested.getPoints().contains(tested.getFaces().get(5).getPoints().get(3)));
 	}
 	
 	@Test
