c521bf4516e143d8a15c975bb22fb52a5af9b3d0
Skytowz <46870222+Skytowz@users.noreply.github.com>
2020-11-07 20:46:00 +0100

    Change groupedView into Canvas

--- sources-du-projet/src/view/View.java
+++ sources-du-projet/src/view/View.java
@@ -1,9 +1,8 @@
 package view;
 
-import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import modele.modelisation.Figure;
@@ -13,7 +12,7 @@ public class View extends Stage{
 	public View(Figure fig) {
 		Buttons buttons = new Buttons(fig);
 		Explorer models = new Explorer();
-		Group display = new GroupedVue(fig);
+		Canvas display = new GroupedVue(fig,500.0,500.0);
 		
 		VBox right = new VBox(); right.setStyle("-fx-border-width: 2px; -fx-border-color: black;");
 		right.getChildren().addAll(display, buttons);
