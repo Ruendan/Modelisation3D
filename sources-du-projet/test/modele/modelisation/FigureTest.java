64af5e5a53310640faf54844b788d263c2effcd4
Dubois Quentin <quentin.dubois3.etu@univ-lile.fr>
2020-11-10 15:45:45 +0100

    All tests of Figures until zoom.

--- sources-du-projet/test/modele/modelisation/FigureTest.java
+++ sources-du-projet/test/modele/modelisation/FigureTest.java
@@ -2,13 +2,61 @@ package modele.modelisation;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import modele.parser.exception.PlyParserException;
 
 class FigureTest {
 
+	private Figure cube;
+	
+	@BeforeEach
+	void initialise() {
+		try {
+			cube = new Figure("cube");
+		} catch (PlyParserException e) {
+			e.printStackTrace();
+		}
+	}
+	
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void test_getCenter() {
+		assertEquals(new Point(0,0,0),cube.getCenter());
+	}
+	
+	@Test
+	void test_getNbFaces() {
+		assertEquals(6, cube.getNbFaces());
+	}
+	
+	@Test
+	void test_getName() {
+		assertEquals("cube", cube.getName());
+	}
+	
+	@Test
+	void test_zoom() {
+		int value = 1;
+		checkZoom(value);
+		value++;
+		cube.zoom(value);
+		checkZoom(value);
+	}
+	
+	void checkZoom(int i) {
+		for(Point p : cube.getPoints()){
+			double x = p.getX();
+			double y = p.getY();
+			double z = p.getZ();
+			
+			if(x>0) assertEquals(i, x, 0.1);
+			else assertEquals(-i, x, 0.1);
+			if(y>0) assertEquals(i, y, 0.1);
+			else assertEquals(-i, y, 0.1);
+			if(z>0) assertEquals(i, z, 0.1);
+			else assertEquals(-i, z, 0.1);
+		}
 	}
 
 }
