ac8337b0a2498a6bc67fdb3f42e70ec0fdef6ab6
quentin.dubois3.etu <quentin.dubois3.etu@univ-lille.fr>
2020-11-11 14:37:40 +0100

    UTF-8

--- sources-du-projet/src/modele/parser/exception/ElementPropertiesError.java
+++ sources-du-projet/src/modele/parser/exception/ElementPropertiesError.java
@@ -7,7 +7,7 @@ public class ElementPropertiesError extends PlyParserException{
 	 */
 	private static final long serialVersionUID = 1L;
 	
-	private static final String classic = "Les propriétés 'element' ne sont pas conformes.";
+	private static final String classic = "Les propriÃ©tÃ©s 'element' ne sont pas conformes.";
 	
 	public ElementPropertiesError() {
 		this(classic);
--- sources-du-projet/src/modele/parser/exception/UnsupportedFileFormat.java
+++ sources-du-projet/src/modele/parser/exception/UnsupportedFileFormat.java
@@ -7,7 +7,7 @@ public class UnsupportedFileFormat extends PlyParserException{
 	 */
 	private static final long serialVersionUID = 1L;
 	
-	private static final String classic = "Le format de ce fichier n'est pas supporté.";
+	private static final String classic = "Le format de ce fichier n'est pas supportÃ©.";
 	
 	public UnsupportedFileFormat() {
 		this(classic);
