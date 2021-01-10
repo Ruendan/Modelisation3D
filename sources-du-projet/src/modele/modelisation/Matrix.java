45434fb2a3a012be2106e78e581dc1f6ae24c012
lucas.hottin.etu <lucas.hottin.etu@univ-lille.fr>
2021-01-10 19:59:46 +0100

    docs matrix

--- sources-du-projet/src/modele/modelisation/Matrix.java
+++ sources-du-projet/src/modele/modelisation/Matrix.java
@@ -1,29 +1,61 @@
 package modele.modelisation;
 
+import com.sun.org.apache.bcel.internal.generic.POP;
+
+import sun.security.util.Length;
+
 public class Matrix {
 	
 	private Double[][] matrice;
-	public static final Matrix MATRICETRANSFORMATION = new Matrix( new Double[][]{
+	
+	
+	private static final Matrix MATRICETRANSFORMATION = new Matrix( new Double[][]{
 		{1.0,	0.0,	0.0},
 		{0.0,	1.0,	0.0}});
 	
+	/**
+	 * Create the matrix of a {@link Point}
+	 * @param p
+	 * 		The {@link Point} which is use for create the matrix
+	 */
 	public Matrix(Point p){
 		this.matrice = new Double[][]{{p.getX(), p.getY(), p.getZ()}};
 	}
 	
+	/**
+	 * Create a matrix from an array of {@link Double}
+	 * @param matrice
+	 * 		The array of {@link Double} which is use for create the matrix
+	 */
 	public Matrix(Double[][] matrice){
 		this.matrice = matrice;
 	}
 	
+	/**
+	 * Get the length of the matrix
+	 * @return
+	 * 		Return the length of the matrix
+	 */
 	public int length() {
 		return matrice.length;
 	}
 	
+	/**
+	 * Get the matrix
+	 * @return
+	 * 		The matrix
+	 */
 	public Double[][] getMatrix(){
 		return matrice;
 	}
 	
-	
+	/**
+	 * Rotate a {@link Point} in the Z axis
+	 * @param point2
+	 * 		the {@link Point} we are going to rotate
+	 * @param theta
+	 * 		the angle of the rotation
+	 */
 	public static void rotateZ(Point point2,double theta) {
 		theta = Math.toRadians(theta);
 		Matrix matrice = new Matrix(new Double[][]{
@@ -34,6 +66,13 @@ public class Matrix {
 		multiplyMatrice(matrice,point2);
 	}
 	
+	/**
+	 * Rotate a {@link Point} in the X axis
+	 * @param point2
+	 * 		the {@link Point} we are going to rotate
+	 * @param theta
+	 * 		the angle of the rotation
+	 */
 	public static void rotateX(Point point2,double theta) {
 		theta = Math.toRadians(theta);
 		Matrix matrice = new Matrix(new Double[][]{
@@ -44,7 +83,13 @@ public class Matrix {
 		multiplyMatrice(matrice,point2);
 	}
 	
-	
+	/**
+	 * Rotate a {@link Point} in the Y axis
+	 * @param point2
+	 * 		the {@link Point} we are going to rotate
+	 * @param theta
+	 * 		the angle of the rotation
+	 */
 	public static void rotateY(Point point2,double theta) {
 		theta = Math.toRadians(theta);
 		Matrix matrice = new Matrix(new Double[][]{
@@ -54,6 +99,7 @@ public class Matrix {
 		
 		multiplyMatrice(matrice,point2);
 	}
+	
 	
 	private static void multiplyMatrice(Matrix matrice, Point point2) {
 		Double[] res = new Double[matrice.length()];
@@ -72,6 +118,10 @@ public class Matrix {
 		if(res.length==3) point2.setZ(res[2]); 
 	}
 	
+	/**
+	 * transfers the point of a 3d space to a 2d plane
+	 * @param point2
+	 */
 	public static void transformation(Point point2) {
 		multiplyMatrice(MATRICETRANSFORMATION,point2);
 	}
