a6089e40e77d0abbf82dc4fce314bd96c74720e6
lucas.hottin.etu <lucas.hottin.etu@univ-lille.fr>
2021-01-10 20:25:45 +0100

    javadoc Point

--- sources-du-projet/src/modele/modelisation/Point.java
+++ sources-du-projet/src/modele/modelisation/Point.java
@@ -5,47 +5,107 @@ public class Point {
 	private double y;
 	private double z;
 	
+	/**
+	 * Get the X coordinate of the point
+	 * @return
+	 * 		The X coordinate
+	 */
 	public Double getX() {
 		return x;
 	}
 
+	/**
+	 * Get the Y coordinate of the point
+	 * @return
+	 * 		The Y coordinate
+	 */
 	public Double getY() {
 		return y;
 	}
 
+	/**
+	 * Get the Z coordinate of the point
+	 * @return
+	 * 		The Z coordinate
+	 */
 	public Double getZ() {
 		return z;
 	}
 	
+	/**
+	 * Set the X coordinate
+	 * @param x
+	 * 		The new value of x
+	 */
 	public void setX(double x) {
 		this.x = x;
 	}
 
+	/**
+	 * Set the Y coordinate
+	 * @param y
+	 * 		The new value of y
+	 */
 	public void setY(double y) {
 		this.y = y;
 	}
 
+	/**
+	 * Set the Z coordinate
+	 * @param z
+	 * 		The new value of z
+	 */
 	public void setZ(double z) {
 		this.z = z;
 	}
 	
+	/**
+	 * Move the point
+	 * @param x
+	 * 		Moving Value of X
+	 * @param y
+	 * 		Moving Value of Y
+	 * @param z
+	 * 		Moving Value of Z
+	 */
 	public void deplacer(double x,double y,double z) {
 		this.x+=x;
 		this.y+=y;
 		this.z+=z;
 	}
 	
+	/**
+	 * Zoom the point
+	 * @param multi
+	 * 		Zooming value
+	 */
 	public void agrandir(double multi) {
 		this.x*=multi;
 		this.y*=multi;
 		this.z*=multi;
 	}
 	
+	/**
+	 * Create a Point
+	 * @param x
+	 * 		The X coordinate
+	 * @param y
+	 * 	 	The Y coordinate
+	 * @param z
+	 * 		The Z coordinate
+	 */
 	public Point(double x, double y, double z) {
 		this.x=x;
 		this.y=y;
 		this.z=z;
 	}
+	/**
+	 * Create a 2D Point
+	 * @param x
+	 * 		The X coordinate
+	 * @param y
+	 * 	 	The Y coordinate
+	 */
 	public Point(double x, double y) {
 		this.x=x;
 		this.y=y;
@@ -57,39 +117,80 @@ public class Point {
 		return "x:" + x + "y:" + y + "z:" + z + ";";
 	}
 
+	/*
+	 * Set new coordinate of a Point with an other Point
+	 */
 	public void setPoint(Point point) {
 		this.x = point.getX();
 		this.y = point.getY();
 		this.z = point.getZ();
 	}
 	
-	
+	/**
+	 * Rotate the Point
+	 * @param thetaX
+	 * 		The angle of rotation of the X axis
+	 * @param thetaY
+	 * 		The angle of rotation of the Y axis
+	 * @param thetaZ
+	 * 		The angle of rotation of the Z axis
+	 */
 	public void rotate(double thetaX, double thetaY, double thetaZ) {
 		Matrix.rotateX(this, thetaX);
 		Matrix.rotateY(this, thetaY);
 		Matrix.rotateZ(this, thetaZ);		
 	}
 	
+	/**
+	 * Rotate the point on the X axis
+	 * @param theta
+	 * 		The angle of rotation
+	 */
 	public void rotateX(double theta) {
 		Matrix.rotateX(this, theta);
 	}
 	
+	/**
+	 * Rotate the point on the Y axis
+	 * @param theta
+	 * 		The angle of rotation
+	 */
 	public void rotateY(double theta) {
 		Matrix.rotateY(this, theta);
 	}
 	
+	/**
+	 * Rotate the point on the Z axis
+	 * @param theta
+	 * 		The angle of rotation
+	 */
 	public void rotateZ(double theta) {
 		Matrix.rotateZ(this, theta);
 	}
 	
+	/**
+	 * Move the point on the X axis
+	 * @param x
+	 * 		The moving value
+	 */
 	public void deplacerX(double x) {
 		this.x+=x;
 	}
 	
+	/**
+	 * Move the point on the Y axis
+	 * @param y
+	 * 		The moving value
+	 */
 	public void deplacerY(double y) {
 		this.y+=y;
 	}
 	
+	/**
+	 * Move the point on the Z axis
+	 * @param z
+	 * 		The moving value
+	 */
 	public void deplacerZ(double z) {
 		this.z+=z;
 	}
@@ -126,6 +227,15 @@ public class Point {
 		return true;
 	}
 	
+	/**
+	 * Set new coordinate of the point
+	 * @param x
+	 * 		new X
+	 * @param y
+	 * 		new Y
+	 * @param z
+	 * 		new Z
+	 */
 	public void setPoint(Double x, Double y, Double z) {
 		this.x = x;
 		this.y = y;
